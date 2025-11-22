74533790c1a8cbefab8bcb3bb747f8bde59d88c8
rowan <u6511498@au.edu>
2025-11-23 00:29:24 +0700

    back to original

--- server.js
+++ server.js
@@ -1,67 +1,35 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import { initDB } from "./config/db.js";
-// import rateLimiter from "./middleware/rateLimiter.js";
-// import transactionsRoute from "./routes/transactionsRoute.js";
-// import cors from "cors";
-
-// dotenv.config();
-
-// const app = express();
-
-// //middleware
-// app.use(rateLimiter);
-// app.use(express.json());
-// app.use(cors());
-
-// //custom simple middleware
-// app.use((req, res, next) => {
-//   console.log("Hey we hit a request,the method is ", req.method);
-//   next();
-// });
-
-// const PORT = process.env.PORT || 5001;
-
-// app.get("/", (req, res) => {
-//   res.send("Server is running");
-// });
-
-// app.use("/api/transactions", transactionsRoute);
-
-// initDB().then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`Server is up and running on PORT:${PORT}`);
-//   });
-// });
-
-// server.js
 import express from "express";
 import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
 import cors from "cors";
-import { VercelRequest, VercelResponse } from "@vercel/node";
 
 dotenv.config();
 
 const app = express();
 
-// Middleware
+//middleware
 app.use(rateLimiter);
 app.use(express.json());
 app.use(cors());
+
+//custom simple middleware
 app.use((req, res, next) => {
-  console.log("Hit request:", req.method, req.url);
+  console.log("Hey we hit a request,the method is ", req.method);
   next();
 });
 
-// Routes
-app.get("/", (req, res) => res.send("Server is running"));
+const PORT = process.env.PORT || 5001;
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
 app.use("/api/transactions", transactionsRoute);
 
-// Initialize DB
-await initDB();
-
-// Export handler for Vercel
-export default app;
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is up and running on PORT:${PORT}`);
+  });
+});
